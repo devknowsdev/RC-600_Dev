208bcb0f2278d90e485624d9108fe760daa66f81
devknowsdev <devknowsdev@gmail.com>
2026-04-20 23:08:27 +0200

    Make TrackCard state-driven

--- apps/rc600-mac-ui/src/features/memory-editor/sections/TrackCard.tsx
+++ apps/rc600-mac-ui/src/features/memory-editor/sections/TrackCard.tsx
@@ -1,13 +1,36 @@
 import { ToggleField } from '../../../components/fields/ToggleField'
 
-export function TrackCard({ trackIndex }: { trackIndex: number }) {
+type Props = {
+  trackIndex: number
+  reverse: boolean
+  oneShot: boolean
+  onToggleReverse: (v: boolean) => void
+  onToggleOneShot: (v: boolean) => void
+}
+
+export function TrackCard({
+  trackIndex,
+  reverse,
+  oneShot,
+  onToggleReverse,
+  onToggleOneShot
+}: Props) {
   return (
     <div style={{ border: '1px solid #ddd', padding: 12, borderRadius: 8, marginBottom: 12 }}>
       <h4 style={{ marginTop: 0 }}>Track {trackIndex}</h4>
 
       <div style={{ display: 'flex', gap: 16 }}>
-        <ToggleField label="Reverse" checked={false} onChange={() => {}} />
-        <ToggleField label="One Shot" checked={false} onChange={() => {}} />
+        <ToggleField
+          label="Reverse"
+          checked={reverse}
+          onChange={onToggleReverse}
+        />
+
+        <ToggleField
+          label="One Shot"
+          checked={oneShot}
+          onChange={onToggleOneShot}
+        />
       </div>
     </div>
   )
